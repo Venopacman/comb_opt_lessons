3fc9e16ec8c6543a986d20deb995ed969077c9a9
Pavel Smirnov <pdsmirnov@yandex.ru>
2018-09-29 23:22:35 +0300

    Local search procedure implemented

--- source/graph_utils.py
+++ source/graph_utils.py
@@ -1,7 +1,6 @@
 import networkx as nx
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import re
 
 
@@ -52,27 +51,19 @@ def plot_graph_nodes(graph: nx.Graph):
     plt.show()
 
 
-def _get_color_shape_tuple_iterator():
-    """
-    Return iterator for color - shape tuple list
-    :return:
-    """
-    import itertools
-    return iter(
-        itertools.product(['b', 'g', 'r', 'c', 'm', 'y', 'k'],
-                          ['o'] * 3))
-
-
 def plot_routes(graph: nx.Graph, route_list):
     pos = dict(zip(graph.nodes, zip(*[[it[1] for it in graph.nodes.data(key)] for key in ("x", "y")])))
     color_map = plt.cm.get_cmap('cubehelix')
     # print(color_map)
+    nx.draw_networkx_nodes(graph, nodelist=[0], pos=pos, node_size=60, node_color=(0.5, 0.5, 0.5))
     for i, route in enumerate(route_list):
-        color = color_map((i + 1) / len(route_list))
+        color = color_map((i + 3) / (len(route_list) + 5))
         # print(color[:-1])
+        # print(color)
         nx.draw_networkx_nodes(graph, nodelist=[it['node'] for it in route[1:-1]], pos=pos, node_size=50,
                                node_color=[color[:-1]] * len(route[1:-1]))
         for ind in range(1, len(route)):
-            nx.draw_networkx_edges(graph, pos=pos, edgelist=[(route[ind]['node'], route[ind-1]['node'])], edge_color=[color])
+            nx.draw_networkx_edges(graph, pos=pos, edgelist=[(route[ind]['node'], route[ind - 1]['node'])],
+                                   edge_color=[color])
     plt.show()
-    print(route_list)
+    # print(route_list)
--- source/heuristics.py
+++ source/heuristics.py
@@ -38,8 +38,17 @@ def calc_cost_metric(deltas, travel_time, feasible_ready_time, feasible_due_time
     return deltas[0] * travel_time + deltas[1] * get_time_diff() + deltas[2] * calc_urgency()
 
 
-def _get_route_with_neighbour_heuristic(graph: nx.Graph, deltas, vehicle_capacity, depo_due_time):
-    def is_route_valid(_route):
+def _get_route_with_neighbour_heuristic(graph: nx.Graph, deltas, vehicle_capacity):
+    """
+    Generate init solution according to Marius M. Solomon
+    A Time-Oriented, Nearest-Neighbor Heuristic algorithm
+    :param graph:
+    :param deltas:
+    :param vehicle_capacity:
+    :return:
+    """
+
+    def is_route_valid_for_nn(_route):
         if (sum([it['demand'] for it in _route]) <= vehicle_capacity) & (
                 all([_route[key]['begin_t'] <= _route[key]['due_t'] for key in [-1, -2]])):
             return True
@@ -59,7 +68,7 @@ def _get_route_with_neighbour_heuristic(graph: nx.Graph, deltas, vehicle_capacit
                                                     graph[route[-2]['node']][cand_node]['time'],
                                                     cand_data['ready_t'], cand_data['due_t'],
                                                     route[-2]['begin_t'], route[-2]['service_t'])
-        # # TODO workaround, logic fix needed
+        # TODO workaround, logic fix needed
         if len(cand_dict.keys()) == 0:
             break
         next_stop_node = min(set(cand_dict.keys()), key=cand_dict.get)
@@ -78,7 +87,7 @@ def _get_route_with_neighbour_heuristic(graph: nx.Graph, deltas, vehicle_capacit
                              {'node': 0,
                               'begin_t': depot_arrival_time,
                               **graph.nodes[0]}]
-        if is_route_valid(feasible_route):
+        if is_route_valid_for_nn(feasible_route):
             route = feasible_route
             filtered_graph.remove_node(next_stop_node)
             # print(next_stop_node)
@@ -101,12 +110,100 @@ class HeuristicSolver:
         self.vehicle_capacity = vehicle_info['capacity']
         self.global_due_time = route_graph.nodes[0]['due_t']
 
+    def calculate_solution_target_metric(self, solution, with_vehicle_amount=True):
+        result = sum([sum([self.route_graph[route[ind - 1]['node']][route[ind]['node']]['time']
+                           for ind in range(1, len(route))]) for route in solution])
+        return result + (len(solution) / self.vehicle_amount) if with_vehicle_amount else result
+
     def get_initial_solution(self):
         route_list = []
         init_graph = self.route_graph.copy()
         while len(init_graph.nodes) > 1:
-            # print(len(init_graph.nodes))
-            new_route, init_graph = _get_route_with_neighbour_heuristic(init_graph, self.deltas, self.vehicle_capacity,
-                                                                        self.global_due_time)
+            new_route, init_graph = _get_route_with_neighbour_heuristic(init_graph, self.deltas, self.vehicle_capacity)
             route_list.append(new_route)
         return route_list
+
+    def get_route_with_reversed_segment(self, _route, i, j):
+        # print("[DEBUG] node_tuple: ", _route[i - 1], _route[i], _route[j - 1], _route[j])
+        resulted_route = _route[:i - 1] + [it for it in reversed(_route[i:j - 1])] + _route[j - 1:]
+        for ind in range(len(resulted_route)):
+            if ind < i:
+                continue
+            else:
+                # TODO check push-forward factor to make it faster (avoid redundant calculations)
+                resulted_route[ind]['begin_t'] = calc_begin_time(resulted_route[ind]['ready_t'],
+                                                                 resulted_route[ind - 1]['begin_t'],
+                                                                 resulted_route[ind - 1]['service_t'],
+                                                                 self.route_graph[resulted_route[ind]['node']][
+                                                                     resulted_route[ind - 1]['node']]['time'])
+        return resulted_route
+
+    def local_search(self, init_solution):
+        """
+
+        :param init_solution:
+        :return:
+        """
+
+        def is_route_valid(_route):
+            return sum([it['demand'] for it in _route]) <= self.vehicle_capacity & all(
+                [_route[ind]['begin_t'] <= _route[ind]['due_t'] for ind in range(len(_route))])
+
+        def _reverse_segment_if_feasible_and_better(_route, i, j):
+            """
+            Route [...(i-1)-i...(j-1)-j...]
+            If reversing route [j:(i-1)] *(equally [i:(j-1)])* would make the route shorter
+                -- then change route in appropriate way.
+            :param _route:
+            :param i:
+            :param j:
+            :return:
+            """
+
+            candidate_route = None
+            a, b, c, d = _route[i - 1]['node'], _route[i]['node'], _route[j - 1]['node'], _route[j]['node']
+            d0 = self.route_graph[a][b]['time'] + self.route_graph[c][d]['time']
+            d1 = self.route_graph[a][c]['time'] + self.route_graph[b][d]['time']
+            if d0 > d1:
+                candidate_route = self.get_route_with_reversed_segment(_route, i, j)
+                is_change_feasible = is_route_valid(candidate_route)
+            else:
+                is_change_feasible = False
+
+            if is_change_feasible:
+                return candidate_route
+
+            else:
+                return _route
+
+        def two_opt(_route):
+            """
+            Iterative improvement based on 2 edges exchange
+            :param _route:
+            :return:
+            """
+
+            def all_segments(n):
+                """
+                Generate all pair nodes combinations
+                :param n:
+                :return:
+                """
+                return [(i, j % n)
+                        for i in range(1, n)
+                        for j in range(i + 2, n)]
+
+            for a, b in all_segments(len(_route)):
+                print(a, b)
+                print(_route[a])
+                print(_route[b])
+                reversed_route = _reverse_segment_if_feasible_and_better(_route, a, b)
+                if reversed_route != _route:
+                    print("[DEBUG] route {0} \n updated to: \n {1}".format(_route, reversed_route))
+                    return two_opt(reversed_route)
+            return _route
+
+        upgraded_route = []
+        for route in init_solution:
+            upgraded_route.append(two_opt(route))
+        return upgraded_route
