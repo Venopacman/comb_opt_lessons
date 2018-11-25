2d9ecde9e53669d89930d4cb942498d746b78c38
Pavel Smirnov <pdsmirnov@yandex.ru>
2018-11-25 23:41:14 +0300

    finalize algorithm + experiments results

--- qap_lab/source/solver.py
+++ qap_lab/source/solver.py
@@ -1,4 +1,5 @@
 import itertools
+import os
 import random
 from copy import deepcopy
 from typing import List
@@ -7,6 +8,11 @@ import numpy as np
 import tqdm
 
 from qap_lab.source.data_utils import Problem
+from tqdm import trange
+
+import json
+import os
+from multiprocessing import Pool
 
 
 class Chromosome:
@@ -23,7 +29,7 @@ class Chromosome:
 
     def calc_fitness(self) -> float:
         n = len(self.genome)
-        return sum([self.flow_matrix[i][j] * self.dist_matrix[self.genome[i]][self.genome[j]]
+        return sum([self.dist_matrix[i][j] * self.flow_matrix[self.genome[i]][self.genome[j]]
                     for i in range(n)
                     for j in range(n)])
 
@@ -32,6 +38,10 @@ class Chromosome:
 
     def scrumble_mutation(self):
         pass
+
+    def persist(self, dir, problem_name):
+        with open(os.path.join(dir, problem_name), 'w') as f:
+            f.write(" ".join([str(it + 1) for it in self.genome]))
 
 
 class Population:
@@ -55,10 +65,9 @@ class Population:
         Calculate inverted related fitness for minimization task
         :return:
         """
-        _sum: float = sum([1 / chrom.fitness for chrom in self.chromosome_list])
-        logits: list = [1 / chrom.fitness for chrom in self.chromosome_list]
-        e_x: np.array = np.exp(np.array(logits) - np.max(logits))
-        return e_x / e_x.sum(axis=0)
+        _sum = sum([1 / chrom.fitness for chrom in self.chromosome_list])
+        probs = [(1 / chrom.fitness) / _sum for chrom in self.chromosome_list]
+        return probs
 
     def select_n_chromosomes(self, n: int) -> List[Chromosome]:
         selected_index = np.random.choice(len(self.chromosome_list), n, p=self.rolling_wheel_prob)
@@ -68,17 +77,18 @@ class Population:
 class GeneticAlgorithmSolver:
     def __init__(self, _problem: Problem):
         self.problem = _problem
-        self.population_size = _problem.problem_size * 10
+        self.population_size = 100
         self.selection_size = int(self.population_size * 0.3)
         self.population = self.generate_initial_population()
 
     def generate_initial_population(self) -> Population:
-        _chromo_list = []
+        _chromo_list = set()
         genome = list(range(self.problem.problem_size))
-        for _ in range(self.population_size):
-            _chromo_list.append(Chromosome(deepcopy(genome), self.problem))
+        while len(_chromo_list) != self.population_size:
+            # for _ in range(self.population_size):
+            _chromo_list.add(Chromosome(deepcopy(genome), self.problem))
             random.shuffle(genome)
-        return Population(_chromo_list, self.problem)
+        return Population(list(_chromo_list), self.problem)
 
     def selection(self, population: Population) -> List[Chromosome]:
         """
@@ -99,26 +109,31 @@ class GeneticAlgorithmSolver:
 
     def reproduction(self, parents: List[Chromosome], n: int) -> List[Chromosome]:
 
-        pairs_universe: List[(Chromosome, Chromosome)] = [(ch_1, ch_2) for (ch_1, ch_2) in
-                                                          itertools.product(parents, parents) if ch_1 != ch_2]
-        pair_sample = [pairs_universe[i] for i in
-                       np.random.choice(len(pairs_universe), n, p=[1 / len(pairs_universe)] * len(pairs_universe))]
-        child_list = []
-        for parent_1, parent_2 in pair_sample:
-            child_list.append(self.ordered_crossover(parent_1, parent_2))
-        return child_list
+        pairs_universe: List[(Chromosome, Chromosome)] = [(ch_1, ch_2) for ch_1 in parents for ch_2 in parents
+                                                          if ch_1 != ch_2]
+        # pair_sample = [pairs_universe[i] for i in
+        #               ]
+        child_list = set()
+        # for parent_1, parent_2 in pair_sample:
+        while len(child_list) != n:
+            parent_1, parent_2 = pairs_universe[
+                np.random.choice(len(pairs_universe), n, p=[1 / len(pairs_universe)] * len(pairs_universe))[0]]
+            child_list.add(self.ordered_crossover(parent_1, parent_2))
+        return list(child_list)
 
     def solve(self) -> Chromosome:
         current_best: Chromosome = self.population.get_best_chromosome()
-        # is_not_ready = True
-        for _ in tqdm.tqdm(range(10000)):
-            parents: List[Chromosome] = self.selection(self.population)
-            childes: List[Chromosome] = self.reproduction(parents, self.population_size - self.selection_size)
+        # t = trange(100, desc='Solving')
+        for _ in range(25000):
+            # avg_fitness = np.average([it.fitness for it in self.population.chromosome_list])
+            # t.set_description('Solving (avg fitness=%g)' % avg_fitness)
+            parents = self.selection(self.population)
+            childes = self.reproduction(parents, self.population_size - self.selection_size)
             self.population = self.mutation(childes, parents)
             cand_best: Chromosome = self.population.get_best_chromosome()
             if cand_best.fitness < current_best.fitness:
                 current_best = cand_best
-                print('Best update: {0}'.format(current_best.fitness))
+                # print('Best update: {0}'.format(current_best.fitness))
         return current_best
 
     def mutation(self, _childes: List[Chromosome], _parents: List[Chromosome]):
@@ -154,7 +169,24 @@ class GeneticAlgorithmSolver:
         return Population(resulted_population, self.problem)
 
 
-if __name__ == "__main__":
-    tai_problem = Problem("../data/tai20a")
+def main(path):
+    problem_name = path.split("/")[-1].split(".")[0]
+    tai_problem = Problem(path)
     genetic_solver = GeneticAlgorithmSolver(tai_problem)
-    print("Final solution score: {0}".format(genetic_solver.solve().fitness))
+    solution = genetic_solver.solve()
+    result_dict = json.load(open("../data/best_results.json"))
+    if result_dict[problem_name] > solution.fitness or result_dict[problem_name] == 0:
+        solution.persist(os.path.dirname(path), problem_name + ".sol")
+        print("Improvement in {0} problem!".format(problem_name))
+        result_dict[problem_name] = int(solution.fitness)
+    print("Problem {0} finished!".format(problem_name))
+    json.dump(result_dict, open("../data/best_results.json", 'w'), indent=2)
+
+
+if __name__ == "__main__":
+    root_dir = '../data'
+    problem_path_list = [os.path.join(root_dir, it) for it in os.listdir(root_dir) if
+                         not (it.endswith(".json") or it.endswith(".sol"))] * 10
+    with Pool(processes=4) as pool:
+        for res in pool.imap_unordered(main, problem_path_list):
+            pass
